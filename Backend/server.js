91b56ef1d679a373003382e6c1cc50264119354c
Carlos Cesin <gojan013@gmail.com>
2025-01-13 01:11:38 -0400

    Unifying Front and backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,28 @@
 import express from 'express';
-import { connectDB } from './config/db.js';
-import materiaRoutes from './routes/materia.route.js';
 import dotenv from 'dotenv';
+import path from 'path';
+
+import { connectDB } from './config/db.js';
+
+import materiaRoutes from './routes/materia.route.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to parse JSON data in the body of the request ( req.body )
 
 app.use("/api/materias", materiaRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+	});
+}
 
 console.log(process.env.MONGO_URL);
 
